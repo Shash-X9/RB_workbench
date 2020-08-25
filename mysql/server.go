ed0a8d4e637b73d6ead37f7ec3f7bc5358c3b87b
V Shaashwath <v.shaashwath@V-Shaashwath-MAC.local>
2020-08-25 13:14:27 +0530

    report table

--- mysql/server.go
+++ mysql/server.go
@@ -21,17 +21,17 @@ func main() {
 		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
 	}))
 
-	type Employee struct {
-		Id     string `json:"id"`
-		Name   string `json:"employee_name"`
-		Salary string `json: "employee_salary"`
-		Age    string `json : "employee_age"`
+	type Report struct {
+		segment_idx  int `json:"segment_id"`
+		Triggered   int`json:"Triggered"`
+		Opened 		int `json:Opened`
+
 	}
 	type Employees struct {
-		Employees []Employee `json:"employee"`
+		Reports []Report `json:"report"`
 	}
 
-	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/user_db")
+	db, err := sql.Open("mysql", "rbrideuser:ridep@s$w0rd@tcp(rides-prod.ccoqqognomd8.ap-southeast-1.rds.amazonaws.com:3306)/ride")
 	if err != nil {
 		fmt.Println(err.Error())
 	} else {
@@ -43,62 +43,63 @@ func main() {
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	e.POST("/employee", func(c echo.Context) error {
-		emp := new(Employee)
-		if err := c.Bind(emp); err != nil {
-			return err
-		}
-		//
-		sql := "INSERT INTO employee(69,employee_name, employee_age, employee_salary) VALUES(69,batman,555,4564478)"
-		stmt, err := db.Prepare(sql)
 
-		if err != nil {
-			fmt.Print(err.Error())
-		}
-		defer stmt.Close()
-		result, err2 := stmt.Exec(emp.Name, emp.Salary, emp.Age)
+	//e.POST("/employee", func(c echo.Context) error {
+	//	emp := new(Employee)
+	//	if err := c.Bind(emp); err != nil {
+	//		return err
+	//	}
+	//	//
+	//	sql := "INSERT INTO employee(employee_name, employee_age, employee_salary) VALUES(?,?,?)"
+	//	stmt, err := db.Prepare(sql)
+	//
+	//	if err != nil {
+	//		fmt.Print(err.Error())
+	//	}
+	//	defer stmt.Close()
+	//	result, err2 := stmt.Exec(emp.Name, emp.Age, emp.Salary)
+	//
+	//	// Exit if we get an error
+	//	if err2 != nil {
+	//		panic(err2)
+	//	}
+	//	fmt.Println(result.LastInsertId())
+	//
+	//	return c.JSON(http.StatusCreated, emp.Name)
+	//})
 
-		// Exit if we get an error
-		if err2 != nil {
-			panic(err2)
-		}
-		fmt.Println(result.LastInsertId())
+	//e.DELETE("/employee/:id", func(c echo.Context) error {
+	//	requested_id := c.Param("id")
+	//	sql := "Delete FROM employee Where id = ?"
+	//	stmt, err := db.Prepare(sql)
+	//	if err != nil {
+	//		fmt.Println(err)
+	//	}
+	//	result, err2 := stmt.Exec(requested_id)
+	//	if err2 != nil {
+	//		panic(err2)
+	//	}
+	//	fmt.Println(result.RowsAffected())
+	//	return c.JSON(http.StatusOK, "Deleted")
+	//})
 
-		return c.JSON(http.StatusCreated, emp.Name)
-	})
-
-	e.DELETE("/employee/:id", func(c echo.Context) error {
-		requested_id := c.Param("id")
-		sql := "Delete FROM employee Where id = ?"
-		stmt, err := db.Prepare(sql)
-		if err != nil {
-			fmt.Println(err)
-		}
-		result, err2 := stmt.Exec(requested_id)
-		if err2 != nil {
-			panic(err2)
-		}
-		fmt.Println(result.RowsAffected())
-		return c.JSON(http.StatusOK, "Deleted")
-	})
-
-	e.GET("/employee/:id", func(c echo.Context) error {
-		requested_id := c.Param("id")
+	e.GET("/report/:id", func(c echo.Context) error {
+		requested_id := c.Param("segment_idx")
 		fmt.Println(requested_id)
-		var name string
-		var id string
-		var salary string
-		var age string
+		var triggered int
+		var segment_idx int
+		var opened int
 
-		err = db.QueryRow("SELECT id,employee_name, employee_age, employee_salary FROM employee WHERE id = ?", requested_id).Scan(&id, &name, &salary, &age)
+
+		err = db.QueryRow("SELECT opened,triggered FROM report WHERE segment_idx = ?", requested_id).Scan(&segment_idx,&triggered, &opened)
 
 		if err != nil {
 			fmt.Println(err)
 		}
 
-		response := Employee{Id: id, Name: name, Salary: salary, Age: age}
+		response := Report{segment_idx: segment_idx, Triggered: triggered, Opened: opened}
 		return c.JSON(http.StatusOK, response)
 	})
 
-	e.Logger.Fatal(e.Start(":3307"))
+	e.Logger.Fatal(e.Start(":3308"))
 }
